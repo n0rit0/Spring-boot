d230cdf14e08c894bca67cc832474be8df1df741
吉村 徳人 <n-yosimura@seassist.co.jp>
2025-01-07 20:03:11 +0900

    add spring boot

---
+++ src/main/java/com/example/demo/fizzBuzz/service/FizzBuzzService.java
@@ -0,0 +1,28 @@
+package com.example.demo.fizzBuzz.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FizzBuzzService {
+
+	public List<String> generateData() {
+		List<String> numbers = new ArrayList<>();
+
+		for (int i = 0; i <= 100; i++) {
+			if (i % 15 == 0) {
+				numbers.add("FizzBuzz");
+			} else if (i % 3 == 0) {
+				numbers.add("Fizz");
+			} else if (i % 5 == 0) {
+				numbers.add("Buzz");
+			} else {
+				numbers.add(String.valueOf(i));
+			}
+		}
+
+		return numbers;
+	}
+}
